d41e0cf9c937a704900511b8130a945248cc5473
Alfarau Ala <alfarau@qlue.id>
2022-11-18 04:53:40 +0700

    add soal1.js and soal2.js

---
+++ soal1.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const number = prompt('Masukan nilai: ');
+
+var squaredRoot;
+
+if(number<=0){
+
+    console.log("Tidak bisa input bilangan negatif")
+
+} else if(number%2!=0) {
+
+    console.log("Tidak bisa input bilangan ganjil")
+
+} else {
+
+    squaredRoot = Math.sqrt(number);
+    console.log("Hasil dari akar " + number + " = " + squaredRoot)
+}
+
